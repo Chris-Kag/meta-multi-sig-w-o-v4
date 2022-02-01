baf98962244861673cca40fd42dd4569d1286f07
Chris Kag <lekag.eth@gmail.com>
2022-02-01 14:59:15 +0200

    test test

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -1,4 +1,7 @@
-export { default as Home } from "./Home";
-export { default as ExampleUI } from "./ExampleUI";
-export { default as Hints } from "./Hints";
+// export { default as Hints } from "./Hints";
+export { default as CreateTransaction } from "./CreateTransaction";
 export { default as Subgraph } from "./Subgraph";
+export { default as Owners } from "./Owners";
+export { default as FrontPage } from "./FrontPage";
+export { default as Transactions } from "./Transactions";
+export { default as Streams } from "./Streams";
